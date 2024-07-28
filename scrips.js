bd3f7102bf25cc57148fca3341cda72f8e041023
wankhairul11 <w.k.aiman1993@gmail.com>
2024-07-28 11:06:19 +0800

    first commit

---
+++ scrips.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__fadeInUp');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.animate__animated').forEach(element => {
+        observer.observe(element);
+    });
+});
